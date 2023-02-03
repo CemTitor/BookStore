cb7937431fed60cb74fd7d39db23ffa8bd322f25
CemYilmaz <cemyilmaz463@gmail.com>
2023-02-04 00:18:54 +0300

    fixed book.cs

--- WebApi/Book.cs
+++ WebApi/Book.cs
@@ -1,10 +1,10 @@
 namespace WebApi;
 
-public class WeatherForecast
+public class Book
 {
     public int Id { get; set; }
 
-    public string? Summary { get; set; }
+    public string? Title { get; set; }
 
     public int GenreId { get; set; }   
 
