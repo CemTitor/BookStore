2d6956f58cdd50cb3c7125e01f47ef313e623657
CemYilmaz <cemyilmaz463@gmail.com>
2023-02-04 00:37:01 +0300

    BookController and HttpGet added

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers;
@@ -6,9 +7,30 @@ namespace WebApi.Controllers;
 [Route("[controller]s")]
 public class BookController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    private static List<Book> BookList = new List<Book>(){
+
+        new Book{
+            Id =1,
+            Title="The Fountainhead",
+            GenreId=1,
+            PageCount=500,
+            PublishDate=DateTime.Now.AddYears(-10), 
+        },
+        new Book{
+            Id =2,
+            Title="Herland",
+            GenreId=2,
+            PageCount=250,
+            PublishDate=DateTime.Now.AddYears(-10), 
+        },
+        new Book{
+            Id =3,
+            Title="Dune",
+            GenreId=2,
+            PageCount=600,
+            PublishDate=DateTime.Now.AddYears(-10), 
+        }
+
     };
 
     private readonly ILogger<BookController> _logger;
@@ -18,15 +40,26 @@ public class BookController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    // [HttpGet]
+    // public List<Book> GetBooks()
+    // {
+    //     var bookList= BookList.OrderBy(x => x.Id).ToList<Book>();
+    //     return bookList;
+    // }
+
+    [HttpGet("{id}")]
+    public Book GetById(int id)
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        var book= BookList.Where(book => book.Id == id).SingleOrDefault();
+        return book;
     }
+   
+   [HttpGet]
+    public Book Get([FromQuery] string id)
+    {
+        var book= BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
+        return book;
+    }
+    
+
 }
